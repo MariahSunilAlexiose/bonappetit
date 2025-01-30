c6a6e0283b3ce484d0c83ec89084c606c0659289
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-01-30 13:30:31 -0500

    Added icons

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,37 @@
+import ArrowRightStartOnRectangleIcon from "./arrow-right-start-on-rectangle.svg"
+import Bars3Icon from "./bars-3.svg"
+import BoxesIcon from "./boxes.svg"
+import BusinessAndFinanceIcon from "./business-and-finance.svg"
+import ChartBarSquareIcon from "./chart-bar-square.svg"
+import ChevronLeftIcon from "./chevron-left.svg"
+import ChevronRightIcon from "./chevron-right.svg"
+import ChevronUpDownIcon from "./chevron-up-down.svg"
+import ClipboardDocumentListIcon from "./clipboard-document-list.svg"
+import CurrencyDollarIcon from "./currency-dollar.svg"
+import EllipsisHorizontalIcon from "./ellipsis-horizontal.svg"
+import KnifeForkIcon from "./knifefork.svg"
+import LogoIcon from "./logo.png"
+import PlusIcon from "./plus.svg"
+import RateIcon from "./rate.svg"
+import UsersIcon from "./users.svg"
+import WorkerIcon from "./worker.svg"
+
+export {
+  ChevronUpDownIcon,
+  PlusIcon,
+  RateIcon,
+  KnifeForkIcon,
+  BoxesIcon,
+  WorkerIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EllipsisHorizontalIcon,
+  LogoIcon,
+  ArrowRightStartOnRectangleIcon,
+  Bars3Icon,
+  BusinessAndFinanceIcon,
+  ChartBarSquareIcon,
+  ClipboardDocumentListIcon,
+  CurrencyDollarIcon,
+  UsersIcon,
+}
