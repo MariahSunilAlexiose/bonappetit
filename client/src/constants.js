5773f2109b29300dceff87d5a6d45d419f3d7a9c
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-01-31 20:47:28 -0500

    Created constants file

---
+++ client/src/constants.js
@@ -0,0 +1,40 @@
+export const keyMapping = {
+  restaurantID: "Restaurant ID",
+  name: "Name",
+  address: "Address",
+  phone: "Phone",
+  rating: "Rating",
+  customerID: "Customer ID",
+  email: "Email",
+  employeeID: "Employee ID",
+  role: "Role",
+  salary: "Salary",
+  inventoryID: "Inventory ID",
+  quantity: "Quantity",
+  unitPrice: "Unit Price",
+  supplierID: "Supplier ID",
+  contactperson: "Contact Person",
+  restaurantName: "Restaurant",
+  supplierName: "Supplier",
+  menuitemID: "Menu Item ID",
+  price: "Price",
+  description: "Description",
+  inventoryOrderID: "Inventory Order ID",
+  date: "Date",
+  paymentStatus: "Payment Status",
+  deliveryStatus: "Delivery Status",
+  employeeName: "Employee",
+  customerOrderID: "Customer Order ID",
+  menuItemName: "Menu Item",
+}
+
+export const paymentstatus = {
+  pending: "Pending",
+  completed: "Completed",
+}
+
+export const deliverystatus = {
+  notdelivered: "Not Delivered",
+  intransit: "In Transit",
+  delivered: "Delivered",
+}
